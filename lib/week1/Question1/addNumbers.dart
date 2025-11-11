81a47f1ddd51f3907373bc6ef9a404e5cf85e1b5
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-11-11 22:09:53 +0430

    Second Week First Question is finished

--- lib/week1/Question1/addNumbers.dart
+++ lib/week1/Question1/addNumbers.dart
@@ -1,22 +1,5 @@
-import 'package:test/test.dart';
-import 'test.dart';
 
-void main() {
-  group('addNumbers', () {
-    test('should correctly add two positive numbers', () {
-      expect(addNumbers(10, 5), equals(15));
-    });
-
-    test('should correctly add a positive and negative number', () {
-      expect(addNumbers(10, -5), equals(5));
-    });
-
-    test('should correctly add two negative numbers', () {
-      expect(addNumbers(-3, -7), equals(-10));
-    });
-
-    test('should correctly add decimal numbers', () {
-      expect(addNumbers(2.5, 3.1), closeTo(5.6, 0.0001));
-    });
-  });
+double addNumbers(double a, double b) {
+  double result = a + b;
+  return result;
 }
---
+++ lib/week1/README.md
@@ -0,0 +1,2 @@
+    SkyTeams_Interns_Fall_Farhad Akbary  
+    The First Week tasks have been finished
