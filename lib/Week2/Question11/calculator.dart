65fb59bc836266b43d00d74f5beb9fa2aff072c0
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-11-12 22:18:59 +0430

    Second Week Questions

--- lib/Week2/Question11/calculator.dart
+++ lib/Week2/Question11/calculator.dart
@@ -3,3 +3,20 @@ Week 2
 11/11/2025
 11. Simple Calculator using switch-case
  */
+double simpleCalculator(double num1, double num2, String operator) {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      if (num2 == 0) {
+        throw ArgumentError('Cannot divide by zero');
+      }
+      return num1 / num2;
+    default:
+      throw ArgumentError('Invalid operator. Use +, -, *, or /.');
+  }
+}
