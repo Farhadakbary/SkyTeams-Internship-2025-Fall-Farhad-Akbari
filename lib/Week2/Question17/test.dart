86f4955aa6ac771113c1fdfc1f8562b878d6305e
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-11-16 20:43:20 +0430

    I make more test case for the questions

--- lib/Week2/Question17/test.dart
+++ lib/Week2/Question17/test.dart
@@ -9,4 +9,24 @@ void main(){
 
     expect(sumOfNatural(-4), equals(0));
   });
+  test('Should add natural numbers', (){
+
+    expect(sumOfNatural(1), equals(1));
+  });
+  test('Should add natural numbers', (){
+
+    expect(sumOfNatural(0), equals(0));
+  });
+  test('Should add natural numbers', (){
+
+    expect(sumOfNatural(10), equals(55));
+  });
+  test('Should add natural numbers', (){
+
+    expect(sumOfNatural(100), equals(5050));
+  });
+  test('Very large amount', (){
+    expect(sumOfNatural(100000), equals(100000 * 100001 / 2));
+    print(sumOfNatural(100000));
+  });
 }
