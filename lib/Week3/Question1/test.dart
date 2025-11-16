6ad002f3a093087d387f8cb7e5960b4408cdf869
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-11-16 21:10:29 +0430

    Week3 Question1

--- lib/Week3/Question1/test.dart
+++ lib/Week3/Question1/test.dart
@@ -14,11 +14,6 @@ void main() {
     test('Counts vowels in all uppercase vowels', () {
       expect(countVowels('AEIOU'), 5);
     });
-
-    test('Counts vowels in a string with no vowels', () {
-      expect(countVowels('bcdfg'), 0);
-    });
-
     test('Counts vowels in an empty string', () {
       expect(countVowels(''), 0);
     });
