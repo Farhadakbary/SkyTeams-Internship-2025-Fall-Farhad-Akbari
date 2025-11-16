2d5eb1c1122951b61c36dc869c2c0f76082b8157
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-11-16 21:01:26 +0430

    Add week3 task

---
+++ lib/Week3/Question1/test.dart
@@ -0,0 +1,26 @@
+import 'package:test/test.dart';
+import 'countVowel.dart';
+
+void main() {
+  group('countVowels', () {
+    test('Counts vowels in a normal sentence', () {
+      expect(countVowels('SkyTeams Internship'), 5);
+    });
+
+    test('Counts vowels in all lowercase vowels', () {
+      expect(countVowels('aeiou'), 5);
+    });
+
+    test('Counts vowels in all uppercase vowels', () {
+      expect(countVowels('AEIOU'), 5);
+    });
+
+    test('Counts vowels in a string with no vowels', () {
+      expect(countVowels('bcdfg'), 0);
+    });
+
+    test('Counts vowels in an empty string', () {
+      expect(countVowels(''), 0);
+    });
+  });
+}
