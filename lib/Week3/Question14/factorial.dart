d3bf8afab8149010624de912ca060e518defd092
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-11-17 16:57:58 +0430

    Week3 Question13, 14, 15

---
+++ lib/Week3/Question14/factorial.dart
@@ -0,0 +1,19 @@
+/* Farhad Akbari
+Week 2
+11/17/2025
+14. Find factorial of a number
+Description: Compute N! using iterative approach.
+Example:
+Input:
+5
+Output:
+120
+ */
+int factorail(int number){
+  number=number.abs();
+  int result=1;
+  for(int i=1; i<=number;i++){
+    result *= i;
+  }
+  return result;
+}
---
+++ lib/Week3/Question15/test.dart
@@ -0,0 +1,7 @@
+import 'package:sky/Week3/Question15/armstrong.dart';
+import 'package:test/test.dart';
+void main(){
+  test('It will say that is it an armstrong number', (){
+    expect(armstrong(153), equals(1));
+  });
+}
