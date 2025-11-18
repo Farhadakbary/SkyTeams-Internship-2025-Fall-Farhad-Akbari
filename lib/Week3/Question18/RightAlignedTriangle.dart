9b16a33419ea56c84235059b7d43d33678402d85
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-11-18 20:24:12 +0430

    Week3 Question15,16,17

---
+++ lib/Week3/Question18/RightAlignedTriangle.dart
@@ -0,0 +1,26 @@
+/* Farhad Akbari
+Week 2
+11/18/2025
+18. Pattern Printing – Right-aligned Triangle
+Example:
+Input:
+4
+Output:
+*
+**
+***
+****
+ */
+String printRightAlignedTriangle(int number) {
+  String result = '';
+  for (int i = 1; i <= number; i++) {
+    for (int j = 1; j <= number - i; j++) {
+      result += ' ';
+    }
+    for (int k = 1; k <= i; k++) {
+      result += '*';
+    }
+    result += '\n';
+  }
+  return result;
+}
