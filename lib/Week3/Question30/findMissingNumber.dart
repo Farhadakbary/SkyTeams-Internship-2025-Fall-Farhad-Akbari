ecff33601a20935cbe722b93994367928840862e
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-11-22 15:48:33 +0430

    some issues fixed

--- lib/Week3/Question30/findMissingNumber.dart
+++ lib/Week3/Question30/findMissingNumber.dart
@@ -8,18 +8,3 @@ Input:
 Output:
 3
  */
-int findMissingNumber(List<int> arr) {
-  int n = arr.length + 1;
-
-  int expectedSum = 0;
-  for (int i = 1; i <= n; i++) {
-    expectedSum += i;
-  }
-
-  int actualSum = 0;
-  for (int num in arr) {
-    actualSum += num;
-  }
-
-  return expectedSum - actualSum;
-}
