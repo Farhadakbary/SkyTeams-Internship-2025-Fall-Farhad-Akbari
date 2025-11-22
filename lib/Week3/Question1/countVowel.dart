5698970b82c5d2445305537cf224e8f918681821
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-11-22 14:12:59 +0430

    Some issues fixed

--- lib/Week3/Question1/countVowel.dart
+++ lib/Week3/Question1/countVowel.dart
@@ -8,11 +8,20 @@ Input:
 Output:
 6
  */
+
 int countVowels (String text){
   const vowel= 'aeiouAEIOU';
   int countVowel= 0;
+  bool myContains(String source, String target) {
+    for (int i = 0; i < source.length; i++) {
+      if (source[i] == target) {
+        return true;
+      }
+    }
+    return false;
+  }
   for(int i= 0; i< text.length;i++){
-    if(vowel.contains(text[i])){
+    if(myContains(vowel,text[i])){
       countVowel ++;
     }
   }
