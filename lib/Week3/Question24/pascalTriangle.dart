703b7b8fa3d70b03978a254397080e0a0c78a53b
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-11-25 22:24:27 +0430

    Week3 Question 24, 30

--- lib/Week3/Question24/pascalTriangle.dart
+++ lib/Week3/Question24/pascalTriangle.dart
@@ -12,3 +12,21 @@ Output:
 1 3 3 1
 1 4 6 4 1
  */
+void pascalTriangle(int rows) {
+  List<List<int>> triangle = [];
+  for (int i = 0; i < rows; i++) {
+    List<int> row = [];
+    for (int j = 0; j <= i; j++) {
+      if (j == 0 || j == i) {
+        row.add(1);
+      } else {
+        int value = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        row.add(value);
+      }
+    }
+    triangle.add(row);
+  }
+  for (List<int> row in triangle) {
+    print(row.join(' '));
+  }
+}
--- lib/Week3/Question30/findMissingNumber.dart
+++ lib/Week3/Question30/findMissingNumber.dart
@@ -8,3 +8,14 @@ Input:
 Output:
 3
  */
+int findMissingNumber(List<int> array) {
+  int num = array.length + 1;
+  int totalSum = num * (num + 1) ~/ 2;
+  int actualSum = 0;
+  for (int i = 0; i < array.length; i++) {
+    actualSum += array[i];
+  }
+  int result = totalSum - actualSum;
+  print(result);
+  return result;
+}
