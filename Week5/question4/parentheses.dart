4601c2b11a9716dd3ec97e24e14e5a49869dbdf6
Farhadakbary <168742706+Farhadakbary@users.noreply.github.com>
2025-12-14 20:57:50 +0430

    Week5 question 4,5,6,7,8,9

---
+++ Week5/question4/parentheses.dart
@@ -0,0 +1,40 @@
+/* Farhad Akbari
+12/12/2025
+1. Valid Parentheses
+Problem:
+Given a string containing only '(' , ')' , '{' , '}' , '[' , ']', determine if the string
+is valid.
+A valid string must have brackets closed in the correct order.
+Example:
+Input: s = "()[]{}"
+Output: true
+Input: s = "(]"
+Output: false
+ */
+bool isValidParentheses(String input) {
+  List<String> stack = [];
+  Map<String, String> pairs = {
+    ')': '(',
+    '}': '{',
+    ']': '['
+  };
+  for (var char in input.split('')) {
+    if (pairs.containsValue(char)) {
+      stack.add(char);
+
+    } else if (pairs.containsKey(char)) {
+      if (stack.isEmpty || stack.last != pairs[char]) {
+        return false;
+      }
+      stack.removeLast();
+    }
+  }
+
+  return stack.isEmpty;
+}
+
+void main() {
+  print(isValidParentheses('()[]{}'));
+  print(isValidParentheses('(]'));
+  print(isValidParentheses('([])'));
+}
